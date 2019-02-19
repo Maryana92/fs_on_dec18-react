9b627bb537e18f9ac17a9f49ad76d36a49fd8c96
mgrinko <glukym@gmail.com>
2019-02-19 10:54:58 +0200

    App with state was added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,48 @@
 import React from 'react';
 
-const App = () => (
-  <h1 className="App">
-    React boilerplate
-  </h1>
-);
+const Header = (props) => {
+  console.log('------Rendering Header', props.title);
+  return (
+    <h1 className="Header">
+      {props.title}
+    </h1>
+  );
+};
+
+class App extends React.Component {
+  constructor(...args) {
+    super(...args);
+
+    this.state = {
+      title: 'My app'
+    };
+  }
+
+  handleChange(event) {
+    this.setState({
+      title: event.target.value
+    });
+
+    console.log('------Setting title');
+  };
+
+
+  render() {
+    console.log('------Rendering App', this.state.title);
+
+    return (
+      <div className="App">
+        <Header title={this.state.title} />
+
+        <input
+          type="text"
+          onChange={(event) => {
+            this.handleChange(event)
+          }}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules)/,
-        use: ['babel-loader', 'eslint-loader'],
+        use: ['babel-loader',/* 'eslint-loader'*/],
       },
     ]
   },
